833bffd21fbd178078b814bc80b3adfaf732c619
gabriel2m <gabriel2m.contact@gmail.com>
2025-07-29 20:45:35 -0300

    build: remove axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,5 @@
 import.meta.glob(['../images/**']);
 
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
