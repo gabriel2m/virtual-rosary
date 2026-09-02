162a503085323b6f2040808be4ef65786bc8bf64
gabriel2m <gabriel2m.contact@gmail.com>
2026-09-01 23:03:07 -0300

    feat: add new templates, change all past beads colors on progress and make beads clickable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import Alpine from 'alpinejs';
+import bead1 from './partials/beads/1.html?raw';
+import bead2 from './partials/beads/2.html?raw';
+import bead3 from './partials/beads/3.html?raw';
 
 declare global {
   interface Window {
     Alpine: Alpine.Alpine;
+    beads: {
+      '1': string,
+      '2': string,
+      '3': string,
+    }
   }
 }
+
+window.beads = {
+  '1': bead1,
+  '2': bead2,
+  '3': bead3
+};
 
 window.Alpine = Alpine;
 Alpine.start();
